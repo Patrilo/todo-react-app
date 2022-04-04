c783b4cbea3bd87ade47eddf654aac7e58d29902
Tito maquetas <javier.samper.arias@nttdata.com>
2022-04-04 13:25:21 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,24 @@
-import React, { useState } from "react";
-import "./App.css";
-import Item from "./components/Item";
-import ItemForm from "./components/ItemForm";
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      content: "📘 Aprender React",
-      isCompleted: false
-    },
-    {
-      content: "⚛️ Crear mi primera aplicación",
-      isCompleted: false
-    },
-    {
-      content: "🚀 Subirla a GitHub",
-      isCompleted: false
-    }
-  ]);
-
-  const completeItem = index => {
-    const newItems = [...items];
-    newItems[index].isComplete = !newItems[index].isComplete;
-    setItems(newItems);
-  };
-
-  const addItem = content => {
-    const newItems = [...items];
-    newItems.unshift({ content: content, isCompleted: false });
-    setItems(newItems);
-  };
-
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      <ItemForm addItem={addItem} />
-      <ul className="ItemList">
-        {items.map((item, index) => (
-          <Item
-            key={index}
-            index={index}
-            content={item.content}
-            completeItem={completeItem}
-            isComplete={item.isComplete}
-          />
-        ))}
-      </ul>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
