6f80e435a4a9b4f58644709564ba8eee9e28e16b
PatriLg <plopegon@nttdata.com>
2022-04-04 17:57:39 +0200

    feat(task-button) : New button added

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,14 @@
 import React from "react";
 
+
 const Item = props => {
-  return (
-    <li
-      className={`Item${props.isComplete ? " completed" : ""}`}
-      onClick={() => props.completeItem(props.index)}
-    >
-      {props.content}
-    </li>
-  );
+    return (
+        <li className={`Item${props.isCompleted ? " completed" : ""}`} >
+            <span>{props.content}</span>
+            <button onClick={() => props.completeItem(props.index)} className="Item done">Hecho</button>
+            <button onClick={() => props.itemDelete(props.index)} className="Item delete">Borrar</button>
+        </li>
+    );
 };
 
 export default Item;
